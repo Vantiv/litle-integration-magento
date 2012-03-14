2a7569fb8804a17fc3bc9f43dd8eb7be8de0f8b0
arch85 <archit.agarwal@gmail.com>
2012-03-14 16:25:53 -0400

    can talk via sdk now.

--- app/code/local/Litle/LitlePayment/Model/PaymentLogic.php
+++ app/code/local/Litle/LitlePayment/Model/PaymentLogic.php
@@ -1,4 +1,6 @@
 <?php
+require_once('LitleOnline.php');
+
 class Litle_LitlePayment_Model_PaymentLogic extends Mage_Payment_Model_Method_Cc
 {
 	/**
@@ -25,12 +27,12 @@ class Litle_LitlePayment_Model_PaymentLogic extends Mage_Payment_Model_Method_Cc
 	/**
 	 * can we capture only partial amounts?
 	 */
-	protected $_canCapturePartial       = false;
+	protected $_canCapturePartial       = true;
 
 	/**
 	 * can this method refund?
 	 */
-	protected $_canRefund               = false;
+	protected $_canRefund               = true;
 
 	/**
 	 * can this method void transactions?
@@ -57,13 +59,161 @@ class Litle_LitlePayment_Model_PaymentLogic extends Mage_Payment_Model_Method_Cc
 	 */
 	protected $_canSaveCc = false;
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+// 	public function getAddressInfo($order_info, $addressType)
+// 	{
+// 		$retArray = array();
+// 		$retArray["firstName"] = XMLFields::returnArrayValue($order_info, ($addressType . "_firstname") );
+// 		//$retArray["middleInitial"]= XMLFields::returnArrayValue($hash_in, ($addressType . "_lastname") );
+// 		$retArray["lastName"] = XMLFields::returnArrayValue($order_info, ($addressType . "_lastname") );
+// 		//$retArray["name"] = XMLFields::returnArrayValue($order_info, ($addressType . "_firstname") );
+// 		$retArray["companyName"] = XMLFields::returnArrayValue($order_info, ($addressType . "_company") );
+// 		$retArray["addressLine1"] = XMLFields::returnArrayValue($order_info, ($addressType . "_address_1") );
+// 		$retArray["addressLine2"] = XMLFields::returnArrayValue($order_info, ($addressType . "_address_2") );
+// 		$retArray["city"] = XMLFields::returnArrayValue($order_info, ($addressType . "_city") );
+// 		$retArray["state"] = XMLFields::returnArrayValue($order_info, ($addressType . "_firstname") );
+// 		$retArray["zip"] = XMLFields::returnArrayValue($order_info, ($addressType . "_postcode") );
+// 		//$retArray["country"] = XMLFields::returnArrayValue($order_info, ($addressType . "_country") );
+// 		$retArray["country"] = XMLFields::returnArrayValue($order_info, ($addressType . "_iso_code_2") );
+// 		$retArray["email"] = XMLFields::returnArrayValue($order_info, "email" );
+// 		$retArray["phone"] = XMLFields::returnArrayValue($order_info, "telephone" );
+// 		return $retArray;
+// 	}
+	
+	public function getCreditCardInfo()
+	{
+		//$this->load->model('checkout/order');
+		//$order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
+	
+// 		$retArray = array();
+// 		$retArray["type"] = $this->request->post['cc_type'];
+// 		$retArray["number"] = str_replace(' ', '', $this->request->post['cc_number']);
+// 		//TODO: fix the logic for expDate
+// 		$retArray["expDate"] = $this->request->post['cc_expire_date_month'] . ($this->request->post['cc_expire_date_year']-2000);
+// 		$retArray["cardValidationNum"] = $this->request->post['cc_cvv2'];
+
+		$retArray = array();
+		$retArray["type"] = "VI";
+		$retArray["number"] = "4100000000000001";
+		//TODO: fix the logic for expDate
+		$retArray["expDate"] = "1125";
+		$retArray["cardValidationNum"] = "369";
+			
+		return $retArray;
+	}
+	
+// 	public function getAmountInCorrectFormat($amount) {
+// 		$retVal = str_replace(",", '', $amount);
+// 		$posOfDot = strpos($retVal, ".");
+// 		if($posOfDot != FALSE){
+// 			$retVal = substr($retVal, 0, $posOfDot + 3);
+// 			$retVal = str_replace(".", '', $retVal);
+// 		}
+// 		return $retVal;
+// 	}
+	
+// 	public function send() {
+// 		$this->load->model('checkout/order');
+// 		$order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
+	
+// 		$orderAmountToInsert = $this->getAmountInCorrectFormat($order_info['total']);
+// 		$hash_in = array(
+// 	 					'orderId'=> $order_info['order_id'],
+// 	 					'amount'=> $orderAmountToInsert,
+// 	 					'orderSource'=> "ecommerce",
+// 	 					'billToAddress'=> $this->getAddressInfo($order_info, "payment"),
+// 	 					'shipToAddress'=> $this->getAddressInfo($order_info, "shipping"),
+// 	 					'card'=> $this->getCreditCardInfo(),
+// 		);
+// 		$litleResponseMessagePrefix = "";
+// 		$litleRequest = new LitleOnlineRequest();
+// 		$doingAuth = $this->config->get('litle_transaction') == "auth";
+// 		if($doingAuth) {
+// 			//auth txn
+// 			$response = $litleRequest->authorizationRequest($hash_in);
+// 			$litleResponseMessagePrefix = "LitleAuthTxn: ";
+// 		}
+// 		else {
+// 			//sale txn
+// 			$response = $litleRequest->saleRequest($hash_in);
+// 			$litleResponseMessagePrefix = "LitleCaptureTxn: ";
+// 		}
+	
+// 		$code = XMLParser::getNode($response, "response");
+// 		$litleValidationMessage = XMLParser::getNode($response, "message");
+// 		$litleTxnId = XMLParser::getNode($response, "litleTxnId");
+	
+// 		$json = array();
+// 		if($code == "000") {
+// 			//Success
+// 			if($doingAuth) {
+// 				$orderStatusId = 1; //Pending
+// 			}
+// 			else {
+// 				$orderStatusId = 5; //Processing
+// 			}
+// 			$message = $litleResponseMessagePrefix . $litleValidationMessage . " \n Litle Response Code: " . $code . "\n  Litle Transaction ID: " . $litleTxnId . " \n";
+// 			$json['success'] = $this->url->link('checkout/success', '', 'SSL');
+// 		}
+// 		else if($code == "100" || $code == "101" || $code == "102" || $code == "110"){
+// 			//Need to try again
+// 			$orderStatusId = 10; //Failed
+// 			$litleResponseMessagePrefix = "LitleTxn: ";
+// 			$message = $litleResponseMessagePrefix . $litleValidationMessage . " \n Litle Response Code: " . $code . "\n  Litle Transaction ID: " . $litleTxnId . " \n";
+// 			$json['error'] = "Either your credit card was declined or there was an error. Please try again or contact us for further help.";
+// 		}
+// 		else {
+// 			$xpath = new DOMXPath($response);
+// 			$query = 'string(/litleOnlineResponse/@message)';
+// 			$message = $xpath->evaluate($query);
+// 			$orderStatusId = 8; //Denied
+// 			$json['error'] = "Either your credit card was declined or there was an error. Please try again or contact us for further help.";
+// 		}
+	
+// 		$this->model_checkout_order->confirm(
+// 		$order_info['order_id'],
+// 		$orderStatusId,
+// 		$message,
+// 		true
+// 		);
+	
+// 		$this->response->setOutput(json_encode($json));
+// 	}
+	
+	
+	
+	
+	
+	
+	
+	
 	/**
 	 * this method is called if we are just authorising
 	 * a transaction
 	 */
 	public function authorize (Varien_Object $payment, $amount)
 	{
-
+		//sleep(20);
+		$hash_in = array(
+	 					'orderId'=> "2135",
+	 					'amount'=> "200000",
+	 					'orderSource'=> "ecommerce",
+	 					//'billToAddress'=> $this->getAddressInfo($order_info, "payment"),
+	 					//'shipToAddress'=> $this->getAddressInfo($order_info, "shipping"),
+	 					'card'=> $this->getCreditCardInfo(),
+		);
+		$litleRequest = new LitleOnlineRequest();
+		$response = $litleRequest->authorizationRequest($hash_in);
+		Mage::throwException("LitleTxnId: " . XMLParser::getNode($response, "litleTxnId"));
+		//$litleResponseMessagePrefix = "LitleAuthTxn: ";
 	}
 
 	/**
---
+++ app/code/local/Litle/LitlePayment/Model/Setup.php
@@ -0,0 +1,75 @@
+<?php
+/*
+ * Copyright (c) 2011 Litle & Co.
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+function initialize(){
+
+	$line = array();
+	$handle = @fopen('./litle_SDK_config.ini', "w");
+	if ($handle) {
+		print "Welcome to Litle PHP_SDK" . PHP_EOL;
+		print "Please input your user name: ";
+		$line['user'] = trim(fgets(STDIN));
+		print "Please input your password: ";
+		$line['password'] = trim(fgets(STDIN));
+		print "Please input your merchantId: ";
+		$line['merchantId'] = trim(fgets(STDIN));
+		print "Please choose Litle url from the following list (example: 'cert') or directly input another URL: \nsandbox => https://www.testlitle.com/sandbox/communicator/online \ncert => https://cert.litle.com/vap/communicator/online \nprecert => https://precert.litle.com/vap/communicator/online \nproduction1 => https://payments.litle.com/vap/communicator/online \nproduction2 => https://payments2.litle.com/vap/communicator/online" . PHP_EOL;
+		$url = urlMapper(trim(fgets(STDIN)));
+		$line['url'] = $url;
+		print "Please input the proxy, if no proxy hit enter key: ";
+		$line['proxy'] = trim(fgets(STDIN));
+
+		foreach ($line as $keys => $values){
+			fwrite($handle, $keys .' = '. $values);
+			fwrite($handle, PHP_EOL);
+		}
+		fwrite($handle, "version = '8.10'" .  PHP_EOL);
+		fwrite($handle, "timeout =  65".  PHP_EOL);
+		fwrite($handle, "id =  10".  PHP_EOL);
+		fwrite($handle, "reportGroup = planets".  PHP_EOL);
+		
+	}
+	fclose($handle);
+	print "The Litle configuration file has been generated, the file is located in the lib directory". PHP_EOL;
+
+}
+
+function urlMapper($litleEnv){
+	$litleOnlineCtx = 'vap/communicator/online';
+	if ($litleEnv == "sandbox")
+		return 'https://www.testlitle.com/sandbox/communicator/online';
+	elseif ($litle_env == "cert")
+		return 'https://cert.litle.com/' . $litleOnlineCtx;
+	elseif ($litleEnv == "precert")
+		return 'https://precert.litle.com/' . $litleOnlineCtx;
+	elseif ($litleEnv == "production1")
+		return 'https://payments.litle.com/' . $litleOnlineCtx;
+	elseif ($litleEnv == "production2")
+		return 'https://payments2.litle.com/' . $litleOnlineCtx;
+	else
+		return 'https://www.testlitle.com/sandbox/communicator/online';
+}
+
+initialize();
+
