fd9e6731e52c63cb820af344d4439cd4a573d937
arch85 <archit.agarwal@gmail.com>
2012-03-15 15:45:58 -0400

    capture works! woohoo!

--- app/code/local/Litle/LitlePayment/Model/PaymentLogic.php
+++ app/code/local/Litle/LitlePayment/Model/PaymentLogic.php
@@ -134,12 +134,44 @@ class Litle_LitlePayment_Model_PaymentLogic extends Mage_Payment_Model_Method_Cc
 	 					'card'=> $this->getCreditCardInfo($payment)
 			);
 			$litleRequest = new LitleOnlineRequest();
-			$response = $litleRequest->authorizationRequest($hash_in);
+			$litleResponse = $litleRequest->authorizationRequest($hash_in);
 			//Mage::throwException($response);
-			//Mage::throwException("this is me");
 			//Mage::throwException(XmlParser::getAttribute($response,'litleOnlineResponse','message'));
-			 Mage::throwException(XmlParser::getNode($response,'message'));
+			//Mage::throwException(XmlParser::getNode($response,'message'));
 			//Mage::throwException(XmlParser::getNode($response,'litleTxnId'));
+			if( isset($litleResponse))
+			{
+				$litleResponseCode = XMLParser::getNode($litleResponse,'response');
+				if($litleResponseCode != "000")
+				{
+					// 					$order_status_id = 1;
+					// 					if( $latest_order_history ){
+					// 						$order_status_id = $latest_order_history[0]['order_status_id'];
+					// 					}
+				}
+				else
+				{
+					$payment
+					->setStatus("Approved")
+					->setCcTransId(XMLParser::getNode($litleResponse,'litleTxnId'))
+					->setLastTransId(XMLParser::getNode($litleResponse,'litleTxnId'))
+					->setCcApproval("Approved");
+					//->setAddressResult($result->getAddressResult())
+					//->setPostcodeResult($result->getPostCodeResult())
+					//->setCv2Result($result->getCV2Result())
+					//->setCcCidStatus($result->getTxAuthNo())
+					//->setSecurityKey($result->getSecurityKey());
+				}
+				//$comment = $litleTxtToInsertInComment . ": " . XMLParser::getNode($litleResponse,'message') . " \n ". $this->language->get('text_litle_response_code') . " " . $litleResponseCode . "\n ". $this->language->get('text_litle_transaction_id'). " " . XMLParser::getNode($litleResponse,'litleTxnId');
+
+				// 				$data = array(
+				// 									'order_status_id'=>$order_status_id,
+				// 									'comment'=>$comment
+				// 				);
+
+				// 				$this->model_sale_order->addOrderHistory($order_id, $data);
+				return $this;
+			}
 		}
 	}
 
@@ -149,7 +181,61 @@ class Litle_LitlePayment_Model_PaymentLogic extends Mage_Payment_Model_Method_Cc
 	 */
 	public function capture (Varien_Object $payment, $amount)
 	{
+		$order = $payment->getOrder();
+		if (!empty($order)){
+			// 			$hash_in = array(
+			// 			 					'orderId'=> $order->getIncrementId(),
+			// 			 					'amount'=> ($amount* 100),
+			// 			 					'orderSource'=> "ecommerce",
+			// 								'billToAddress'=> $this->getBillToAddress($payment),
+			// 								'shipToAddress'=> $this->getAddressInfo($payment),
+			// 			 					'card'=> $this->getCreditCardInfo($payment)
+			// 			);
+			$hash_in = array(
+								'litleTxnId' => $payment->getCcTransId()
+			);
+			$litleRequest = new LitleOnlineRequest();
+			$litleResponse = $litleRequest->captureRequest($hash_in);
+			//Mage::throwException($litleResponse->saveXML());
+		}
+			
+		if( isset($litleResponse))
+		{
+			$litleResponseCode = XMLParser::getNode($litleResponse,'response');
+			if($litleResponseCode != "000")
+			{
+				// 					$order_status_id = 1;
+				// 					if( $latest_order_history ){
+				// 						$order_status_id = $latest_order_history[0]['order_status_id'];
+				// 					}
+			}
+			else
+			{
+				$payment
+				->setStatus("Approved")
+				->setCcTransId(XMLParser::getNode($litleResponse,'litleTxnId'))
+				->setLastTransId(XMLParser::getNode($litleResponse,'litleTxnId'))
+				->setTransactionId(XMLParser::getNode($litleResponse,'litleTxnId'))
+				->setIsTransactionClosed(0)
+				->setTransactionAdditionalInfo(XMLParser::getNode($litleResponse,'message'));
+				//->setCcApproval("Approved");
+				// 					->setAddressResult($result->getAddressResult())
+				// 					->setPostcodeResult($result->getPostCodeResult())
+				// 					->setCv2Result($result->getCV2Result())
+				// 					->setCcCidStatus($result->getTxAuthNo())
+				// 					->setSecurityKey($result->getSecurityKey());
+			}
+			
+			return $this;
+			//$comment = $litleTxtToInsertInComment . ": " . XMLParser::getNode($litleResponse,'message') . " \n ". $this->language->get('text_litle_response_code') . " " . $litleResponseCode . "\n ". $this->language->get('text_litle_transaction_id'). " " . XMLParser::getNode($litleResponse,'litleTxnId');
 
+			// 				$data = array(
+			// 									'order_status_id'=>$order_status_id,
+			// 									'comment'=>$comment
+			// 				);
+
+			// 				$this->model_sale_order->addOrderHistory($order_id, $data);
+		}
 	}
 
 	/**
