3c40f7ed7bad58c5a550dbb74a3455c6862774f8
Rushi <rganmukh@litle.com>
2012-03-22 12:39:46 -0400

    cleaned up Credit Card Admin panel

--- app/code/local/Litle/CreditCard/Model/PaymentLogic.php
+++ app/code/local/Litle/CreditCard/Model/PaymentLogic.php
@@ -154,7 +154,7 @@ class Litle_CreditCard_Model_PaymentLogic extends Mage_Payment_Model_Method_Cc
 		$hash = array('user'=> $this->getConfigData("user"),
  					'password'=> $this->getConfigData("password"),
 					'merchantId'=>$this->getConfigData("merchant_id"),
-					'version'=>$this->getConfigData("version"),
+					'version'=>'8.10',
 					'reportGroup'=>$this->getConfigData("reportGroup"),
 					'url'=>$this->getConfigData("url"),	
 					'proxy'=>$this->getConfigData("proxy"),
