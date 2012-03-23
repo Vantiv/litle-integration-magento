50b628bfdc204abb9f185274edc7cc4525601c63
Rushi <rganmukh@litle.com>
2012-03-23 12:00:15 -0400

    will only check http if payment method is enabled

--- app/code/local/Litle/CreditCard/Model/Validatehttp.php
+++ app/code/local/Litle/CreditCard/Model/Validatehttp.php
@@ -7,26 +7,28 @@ class Litle_CreditCard_Model_Validatehttp extends Mage_Core_Model_Config_Data
 		return (is_array($data) && isset($data[$key])) ? $data[$key] : null;
 	}
 	function save(){
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_PROXY, $this->getFieldsetDataValue('proxy'));
-		curl_setopt($ch, CURLOPT_POST, true);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: text/xml'));
-		curl_setopt($ch, CURLOPT_URL, $this->getFieldsetDataValue('url'));
-		curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, true);
-		curl_setopt($ch,CURLOPT_TIMEOUT,'5');
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,2);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		$output = curl_exec($ch);
-		
-		if (! $output){
-			Mage::throwException('Error Connecting to Litle System, Please Make sure your HTTP configuration settings are correct');
-		}
-		else
+		if ($this->getFieldsetDataValue('active'))
 		{
-			curl_close($ch);
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_PROXY, $this->getFieldsetDataValue('proxy'));
+			curl_setopt($ch, CURLOPT_POST, true);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: text/xml'));
+			curl_setopt($ch, CURLOPT_URL, $this->getFieldsetDataValue('url'));
+			curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, true);
+			curl_setopt($ch,CURLOPT_TIMEOUT,'5');
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,2);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			$output = curl_exec($ch);
+
+			if (! $output){
+				Mage::throwException('Error Connecting to Litle System, Please Make sure your HTTP configuration settings are correct');
+			}
+			else
+			{
+				curl_close($ch);
+			}
+			return parent::save();
 		}
-		return parent::save();
-		
 	}
 }
