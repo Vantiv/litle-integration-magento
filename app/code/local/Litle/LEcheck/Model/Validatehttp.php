f052e0a59d470bbcced23f59f280e33c945ddbd3
Rushi <rganmukh@litle.com>
2012-03-23 11:55:06 -0400

    http validation for echeck

---
+++ app/code/local/Litle/LEcheck/Model/Validatehttp.php
@@ -0,0 +1,32 @@
+<?php
+class Litle_LEcheck_Model_Validatehttp extends Mage_Core_Model_Config_Data
+{
+	public function getFieldsetDataValue($key)
+	{
+		$data = $this->_getData('fieldset_data');
+		return (is_array($data) && isset($data[$key])) ? $data[$key] : null;
+	}
+	function save(){
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_PROXY, $this->getFieldsetDataValue('proxy'));
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: text/xml'));
+		curl_setopt($ch, CURLOPT_URL, $this->getFieldsetDataValue('url'));
+		curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, true);
+		curl_setopt($ch,CURLOPT_TIMEOUT,'5');
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,2);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$output = curl_exec($ch);
+		
+		if (! $output){
+			Mage::throwException('Error Connecting to Litle System, Please Make sure your HTTP configuration settings are correct');
+		}
+		else
+		{
+			curl_close($ch);
+		}
+		return parent::save();
+		
+	}
+}
