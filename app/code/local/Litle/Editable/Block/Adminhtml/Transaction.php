36f2ad0e5418a3152d8d93da15859f577953e72f
Jack, Gao <rgao@litle.com>
2012-04-18 17:28:08 -0400

    done with trasaction data screen

--- app/code/local/Litle/Editable/Block/Adminhtml/Transaction.php
+++ app/code/local/Litle/Editable/Block/Adminhtml/Transaction.php
@@ -44,18 +44,35 @@ class Litle_Editable_Block_Adminhtml_Transaction extends Mage_Adminhtml_Block_Sa
 		$url = $litle->getConfigData("url");
 		$litleTxnId = $this->_txn->getTxnId();
 		$txnType = $this->_txn->getTxnType();
-		
-		preg_match("/https:\/\/(\w+)\..*/",$url,$matches);
-		$base_url = $matches[1];
+		preg_match("/(\w+):\/\/([A-Za-z0-9.:]+).*?/",$url,$matches);
+		$protocol = $matches[1];
+		$base_url = $matches[2];
+		$base_url = str_replace("2180","2190", $base_url);
+		$method = $this->_txn->getOrderPaymentObject()->getMethod();
 		
 		if($txnType == 'authorization') {
+			if($method == 'lecheck'){
+				$litleTxnType = 'echeck/verification';
+			}
+			else{
 			$litleTxnType = 'authorization';
+			}
 		}
 		else if($txnType == 'capture') {
+			if($method == 'lecheck'){
+				$litleTxnType = 'echeck/deposit';
+			}
+			else{
 			$litleTxnType = 'deposit';
+			}
 		}
 		else if($txnType == 'refund') {
+			if($method == 'lecheck'){
+				$litleTxnType = 'echeck/refund';
+			}
+			else{
 			$litleTxnType = 'refund';
+			}
 		}
 		else if($txnType == 'void') {
 			if(preg_match("/\d{18}-void/",$litleTxnId) == 0) {
@@ -69,7 +86,12 @@ class Litle_Editable_Block_Adminhtml_Transaction extends Mage_Adminhtml_Block_Sa
 			return parent::getTxnIdHtml();
 		}
 				
-		return "<a href='https://reports.$base_url.litle.com/ui/reports/payments/$litleTxnType/$litleTxnId'>$litleTxnId</a>";
+		$reports = "";
+		if($base_url == 'payments' || $base_url == 'cert' || $base_url == 'precert') {
+			$reports = "reports.";
+		}
+		
+		return "<a href='$protocol://$reports$base_url/ui/reports/payments/$litleTxnType/$litleTxnId'>$litleTxnId</a>";
 	}
 
 }
