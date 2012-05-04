134ab1abbc16872d691092f571ae3212836512d1
Rushi <rganmukh@litle.com>
2012-05-04 16:14:22 -0400

    merchant id map for credit cards complete w/validation

--- app/code/local/Litle/CreditCard/Model/ValidateMerchantId.php
+++ app/code/local/Litle/CreditCard/Model/ValidateMerchantId.php
@@ -6,22 +6,22 @@ class Litle_CreditCard_Model_ValidateMerchantId extends Mage_Core_Model_Config_D
 		$data = $this->_getData('fieldset_data');
 		return (is_array($data) && isset($data[$key])) ? $data[$key] : null;
 	}
+
 	function save(){
 		if ($this->getFieldsetDataValue('active'))
 		{
-			//var_dump($this);exit;
-				//$currency = $this->getFieldsetDataValue('currency');// = "UWD";
-				$currency = "USD";
-				$currency = Mage::getSingleton(creditcard/payment)
-				//Mage::throwException($currency);
-				$string2Eval = 'return array' . $this->getFieldsetDataValue("merchant_id") . ';';
-				$merchant_map = eval($string2Eval);
-					//Mage::throwException(get_class($merchant_map));
-				if (!is_array($merchant_map) || empty($merchant_map[$currency])){
-				
-					Mage::throwException('Merchant ID must be of the form ("Currency" => "Code"), i.e. ("USD" => "101","GBP" => "102")');
-				}
+			$currency = "USD";
+			$string2Eval = 'return array' . $this->getFieldsetDataValue("merchant_id") . ';';
+			$merchant_map = eval($string2Eval);
+
+			if(!is_array($merchant_map)){
+				Mage::throwException('Merchant ID must be of the form ("Currency" => "Code"), '. PHP_EOL . 'i.e. ("USD" => "101","GBP" => "102")');
 			}
-			return parent::save();
+			if(empty($merchant_map[$currency])){
+				Mage::throwException('Please Make sure that the Base Currency: ' . $currency . ' is in the Merchant ID Array');
+			}
+
 		}
+		return parent::save();
 	}
+}
