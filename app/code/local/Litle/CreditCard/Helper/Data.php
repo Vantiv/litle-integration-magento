a533b0a041a82c08938e3d7d345875aec77481a5
arch85 <archit.agarwal@gmail.com>
2012-05-09 15:54:03 -0400

    Adding helper to Credit Card.

--- app/code/local/Litle/CreditCard/Helper/Data.php
+++ app/code/local/Litle/CreditCard/Helper/Data.php
@@ -1,5 +1,28 @@
 <?php
 class Litle_CreditCard_Helper_Data extends Mage_Core_Helper_Abstract
 {
-
+	public function canDo($order, $typeToDo){
+		$payment = $order->getPayment();
+		$lastTxnId = $payment->getLastTransId();
+		$lastTxn = $payment->getTransaction($lastTxnId);
+		
+		if( $lastTxn->getTxnType() === $typeToDo )
+			return true;
+		else
+			return false;
+	}
+	
+	// TODO:: Needs to be implemented.
+	public function isMOPLitleCC(){
+		return true;
+	}
+	
+	// TODO:: Needs to be implemented.
+	public function isMOPLitleECheck(){
+		return true;
+	}
+	
+	public function isMOPLitle(){
+		return ($this->isMOPLitleCC() || $this->isMOPLitleECheck());
+	}
 }
