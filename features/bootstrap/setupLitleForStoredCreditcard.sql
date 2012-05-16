141eab3248eff98711a3da10af3730b9c127a8fd
Kush, Patel <kpatel@litle.com>
2012-05-16 15:37:39 -0400

    set greg's id based on search from vault (not hardcoded anymore)

--- features/bootstrap/setupLitleForStoredCreditcard.sql
+++ features/bootstrap/setupLitleForStoredCreditcard.sql
@@ -1,1 +1,2 @@
-insert into vault values(2,6,2,'5555','1234123412345555','MC','41232');
+set @gregID = (select customer_id from vault);
+insert into vault values(2,6,@gregID,'5555','1234123412345555','MC','41232');
