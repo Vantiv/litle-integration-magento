41520a7f9ef6a3740380ef886364bf8786a3a55e
arch85 <archit.agarwal@gmail.com>
2012-05-24 10:34:12 -0400

    wip magento plugin builder

--- build/src/com/litle/magento/builder/LitleMagentoXMLWriter.java
+++ build/src/com/litle/magento/builder/LitleMagentoXMLWriter.java
@@ -41,44 +41,25 @@ public class LitleMagentoXMLWriter {
 			doc.appendChild(rootElement);
 			
 			// root attributes
-			Attr attr = doc.createAttribute("xmlns");
-			attr.setValue("http://pear.php.net/dtd/package-2.0");
-			rootElement.setAttributeNode(attr);
+			setAttribute(rootElement, "xmlns", "http://pear.php.net/dtd/package-2.0");
+			setAttribute(rootElement, "xmlns:tasks", "http://pear.php.net/dtd/tasks-1.0");
+			setAttribute(rootElement, "xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
+			setAttribute(rootElement, "packagerversion", "1.9.1");
+			setAttribute(rootElement, "version", "2.0");
+			setAttribute(rootElement, "xsi:schemaLocation", "http://pear.php.net/dtd/tasks-1.0 http://pear.php.net/dtd/tasks-1.0.xsd http://pear.php.net/dtd/package-2.0 http://pear.php.net/dtd/package-2.0.xsd");
 			
+			// sub elements
+			appendChildElement(rootElement, "name", "Litle_Payments");
+			appendChildElement(rootElement, "channel", "connect.magentocommerce.com/community");
+			appendChildElement(rootElement, "summary", "This extension allows you to accept payments through Litle.");
+			appendChildElement(rootElement, "description", "Installation of this extension will allow you to easily accept payments through Litle. Once installed, you can choose to accept credit cards as well as eChecks to be processed by Litle.&#13;\n&#13;\nYou will need to contact Litle to setup a merchant ID prior to processing your transaction. You can test your system against our sandbox without the need to contact Litle first.&#13;\n&#13;\nTo test with sandbox, you may enter any user name and password, and select 'Sandbox' from the drop down menu in Payment Method configuration.&#13;\n&#13;\nWhy Litle?&#13;\n&#13;\nWe deliver the most efficient and effective core processing available to digital and direct merchants. Relevant, value-added solutions help you drive more lasting and profitable customer relationships. We’ll also show you how payments intelligence can power your business and your relationships to greater success. We support you with the best customer experience in the business.&#13; ");
 			
+			Element leadElement = doc.createElement("lead");
+			appendChildElement(leadElement, "name", "Litle");
+			appendChildElement(leadElement, "user", "user");
+			appendChildElement(leadElement, "sdksupport@litle.com", "email");
+			appendChildElement(leadElement, "active", "yes");
 			
-			// staff elements
-			Element staff = doc.createElement("Staff");
-			rootElement.appendChild(staff);
-
-			// set attribute to staff element
-			Attr attr = doc.createAttribute("id");
-			attr.setValue("1");
-			staff.setAttributeNode(attr);
-
-			// shorten way
-			// staff.setAttribute("id", "1");
-
-			// firstname elements
-			Element firstname = doc.createElement("firstname");
-			firstname.appendChild(doc.createTextNode("yong"));
-			staff.appendChild(firstname);
-
-			// lastname elements
-			Element lastname = doc.createElement("lastname");
-			lastname.appendChild(doc.createTextNode("mook kim"));
-			staff.appendChild(lastname);
-
-			// nickname elements
-			Element nickname = doc.createElement("nickname");
-			nickname.appendChild(doc.createTextNode("mkyong"));
-			staff.appendChild(nickname);
-
-			// salary elements
-			Element salary = doc.createElement("salary");
-			salary.appendChild(doc.createTextNode("100000"));
-			staff.appendChild(salary);
-
 			// write the content into xml file
 			TransformerFactory transformerFactory = TransformerFactory
 					.newInstance();
@@ -105,6 +86,12 @@ public class LitleMagentoXMLWriter {
 		Attr attr = doc.createAttribute(attrName);
 		attr.setValue(attrVal);
 		in_element.setAttributeNode(attr);
+	}
+	
+	public void appendChildElement(Element parentElement, String childName, String childVal) {
+		Element elemToAdd = doc.createElement(childName);
+		elemToAdd.appendChild(doc.createTextNode(childVal));
+		parentElement.appendChild(elemToAdd);
 	}
 
 	public static void main(String argv[]) {
