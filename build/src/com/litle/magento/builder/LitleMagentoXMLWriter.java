f5833af5e8fc6f13de3713912f8299beb5654674
arch85 <archit.agarwal@gmail.com>
2012-05-29 12:33:20 -0400

    Magento plugin build xml writer complete.

--- build/src/com/litle/magento/builder/LitleMagentoXMLWriter.java
+++ build/src/com/litle/magento/builder/LitleMagentoXMLWriter.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.security.MessageDigest;
+import java.text.SimpleDateFormat;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -14,6 +15,7 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
+import java.util.Date;
 import java.util.HashMap;
 
 import org.w3c.dom.Attr;
@@ -66,74 +68,83 @@ public class LitleMagentoXMLWriter {
 			Element rootElement = doc.createElement("package");
 			doc.appendChild(rootElement);
 
-			// root attributes
-			setAttribute(rootElement, "xmlns",
-					"http://pear.php.net/dtd/package-2.0");
-			setAttribute(rootElement, "xmlns:tasks",
-					"http://pear.php.net/dtd/tasks-1.0");
-			setAttribute(rootElement, "xmlns:xsi",
-					"http://www.w3.org/2001/XMLSchema-instance");
-			setAttribute(rootElement, "packagerversion", "1.9.1");
-			setAttribute(rootElement, "version", "2.0");
-			setAttribute(
-					rootElement,
-					"xsi:schemaLocation",
-					"http://pear.php.net/dtd/tasks-1.0 http://pear.php.net/dtd/tasks-1.0.xsd http://pear.php.net/dtd/package-2.0 http://pear.php.net/dtd/package-2.0.xsd");
-
+		    String packageName = "Litle_Payments";
+		    String packageVersion = "8.13.0";
+		    String packageStability = "stable";
+		    String packageLicense = "MIT";
+		    String packageChannel = "community";
+		    String packageExtends = "";
+		    String packageSummary = "This extension allows you to accept payments through Litle.";
+		    String packageDescription = "Installation of this extension will allow you to easily accept payments through Litle. Once installed, you can choose to accept credit cards as well as eChecks to be processed by Litle.&#13;\n&#13;\nYou will need to contact Litle to setup a merchant ID prior to processing your transaction. You can test your system against our sandbox without the need to contact Litle first.&#13;\n&#13;\nTo test with sandbox, you may enter any user name and password, and select 'Sandbox' from the drop down menu in Payment Method configuration.&#13;\n&#13;\nWhy Litle?&#13;\n&#13;\nWe deliver the most efficient and effective core processing available to digital and direct merchants. Relevant, value-added solutions help you drive more lasting and profitable customer relationships. We’ll also show you how payments intelligence can power your business and your relationships to greater success. We support you with the best customer experience in the business.&#13; ";
+		    String packageNotes = "This extension implements Litle XML version 8.12&#13;\n&#13;\nAdditional features include enhanced reporting on orders, transactions, and customers.";
+		    
+		    String authorName = "Litle";
+			String authorUser = "Litle";
+			String authorEmail = "sdksupport@litle.com";
+			
+			String packageCompatible = "";
+			String phpMinElement = "5.2.0";
+			String phpMaxElement = "6.0.0";
+		    
 			// sub elements
-			appendChildElement(rootElement, "name", "Litle_Payments");
-			appendChildElement(rootElement, "channel",
-					"connect.magentocommerce.com/community");
-			appendChildElement(rootElement, "summary",
-					"This extension allows you to accept payments through Litle.");
-			appendChildElement(
-					rootElement,
-					"description",
-					"Installation of this extension will allow you to easily accept payments through Litle. Once installed, you can choose to accept credit cards as well as eChecks to be processed by Litle.&#13;\n&#13;\nYou will need to contact Litle to setup a merchant ID prior to processing your transaction. You can test your system against our sandbox without the need to contact Litle first.&#13;\n&#13;\nTo test with sandbox, you may enter any user name and password, and select 'Sandbox' from the drop down menu in Payment Method configuration.&#13;\n&#13;\nWhy Litle?&#13;\n&#13;\nWe deliver the most efficient and effective core processing available to digital and direct merchants. Relevant, value-added solutions help you drive more lasting and profitable customer relationships. We’ll also show you how payments intelligence can power your business and your relationships to greater success. We support you with the best customer experience in the business.&#13; ");
-
-			Element leadElement = doc.createElement("lead");
-			appendChildElement(leadElement, "name", "Litle");
-			appendChildElement(leadElement, "user", "user");
-			appendChildElement(leadElement, "email", "sdksupport@litle.com");
-			appendChildElement(leadElement, "active", "yes");
-			rootElement.appendChild(leadElement);
-
-			// TODO :: populate date and time dynamically
-			appendChildElement(rootElement, "date", "2012-05-23");
-			appendChildElement(rootElement, "time", "15:35:25");
-
-			// TODO :: grab release and api version dynamically
-			Element versionElement = doc.createElement("version");
-			appendChildElement(versionElement, "release", "8.12.0");
-			appendChildElement(versionElement, "api", "8.12.0");
-			rootElement.appendChild(versionElement);
-
-			Element stabilityElement = doc.createElement("stability");
-			appendChildElement(stabilityElement, "release", "stable");
-			appendChildElement(stabilityElement, "api", "stable");
-			rootElement.appendChild(stabilityElement);
-
-			appendChildElement(rootElement, "license", "MIT");
-			appendChildElement(
-					rootElement,
-					"notes",
-					"This extension implements Litle XML version 8.12&#13;\n&#13;\nAdditional features include enhanced reporting on orders, transactions, and customers.");
+			appendChildElement(rootElement, "name", packageName);
+			appendChildElement(rootElement, "version", packageVersion);
+			appendChildElement(rootElement, "stability", packageStability);
+			appendChildElement(rootElement, "license", packageLicense);
+			appendChildElement(rootElement, "channel", packageChannel);
+			appendChildElement(rootElement, "extends", packageExtends);
+			appendChildElement(rootElement, "summary", packageSummary);
+			appendChildElement(rootElement,"description",packageDescription);
+			appendChildElement(rootElement,"notes",packageNotes);
+		
+			Element authorsElement = doc.createElement("authors");
+			Element authorElement = doc.createElement("author");
+			appendChildElement(authorElement, "name", authorName);
+			appendChildElement(authorElement, "user", authorUser);
+			appendChildElement(authorElement, "email", authorEmail);
+			authorsElement.appendChild(authorElement);
+			rootElement.appendChild(authorsElement);
+			
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+			SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
+			appendChildElement(rootElement, "date", dateFormat.format(new Date()));
+			appendChildElement(rootElement, "time", timeFormat.format(new Date()));
 
 			Element contentsElement = doc.createElement("contents");
-			Element rootDirElement = doc.createElement("dir");
-			setAttribute(rootDirElement, "name", "/");
-			Element litleDirElement = doc.createElement("dir");
-			setAttribute(litleDirElement, "name", "Litle");
-			Element litleEmptyElement = doc.createElement("dir");
-			setAttribute(litleEmptyElement, "name", "");
+			Element localTargetElement = doc.createElement("target");
+			setAttribute(localTargetElement, "name", "magelocal");
+			Element dirLitleElement = doc.createElement("dir");
+			setAttribute(dirLitleElement, "name", "Litle");
+			Element emptyDirElement = doc.createElement("dir");
 			
-			addNodesFromFileStructureInFolder(litleEmptyElement, "app/code/local/Litle/", "Litle");
+			addNodesFromFileStructureInFolder(emptyDirElement, "app/code/local/Litle/");
 			
-			litleDirElement.appendChild(litleEmptyElement);
-			rootDirElement.appendChild(litleDirElement);
-			contentsElement.appendChild(rootDirElement);
+			dirLitleElement.appendChild(emptyDirElement);
+			localTargetElement.appendChild(dirLitleElement);
+			contentsElement.appendChild(localTargetElement);
+
+			Element etcTargetElement = doc.createElement("target");
+			setAttribute(etcTargetElement, "name", "mageetc");
+			addNodesFromFileStructureInFolder(etcTargetElement, "app/etc/");
+			contentsElement.appendChild(etcTargetElement);
+		
+			Element designTargetElement = doc.createElement("target");
+			setAttribute(designTargetElement, "name", "magedesign");
+			addNodesFromFileStructureInFolder(designTargetElement, "app/design/");
+			contentsElement.appendChild(designTargetElement);
 			
 			rootElement.appendChild(contentsElement);
+			
+			appendChildElement(rootElement, "compatible", packageCompatible);
+			
+			Element dependenciesElement = doc.createElement("dependencies");
+			Element requiredElement = doc.createElement("required");
+			Element phpElement = doc.createElement("php");
+			appendChildElement(phpElement, "min", phpMinElement);
+			appendChildElement(phpElement, "max", phpMaxElement);
+			requiredElement.appendChild(phpElement);
+			dependenciesElement.appendChild(requiredElement);
+			rootElement.appendChild(dependenciesElement);
 			
 			// write the content into xml file
 			TransformerFactory transformerFactory = TransformerFactory
@@ -158,7 +169,7 @@ public class LitleMagentoXMLWriter {
 		return true;
 	}
 
-	public boolean addNodesFromFileStructureInFolder(Element parentElement, String folderName, String originalCall) {
+	public boolean addNodesFromFileStructureInFolder(Element parentElement, String folderName) {
 		boolean retVal = false;
 		File dir = new File(this.pathToLitleMagentoIntegrationFolder + folderName);
 		if (dir.exists()) {
@@ -169,7 +180,7 @@ public class LitleMagentoXMLWriter {
 					if (fileOrDir != null && fileOrDir.isDirectory()) {
 						Element dirElement = doc.createElement("dir");
 						setAttribute(dirElement, "name", fileOrDir.getName());
-						boolean wasSomethingAdded = addNodesFromFileStructureInFolder(dirElement,(folderName + "/" + fileOrDir.getName()), originalCall);
+						boolean wasSomethingAdded = addNodesFromFileStructureInFolder(dirElement,(folderName + "/" + fileOrDir.getName()));
 						if( wasSomethingAdded ){
 							parentElement.appendChild(dirElement);
 							retVal = true;
@@ -179,8 +190,7 @@ public class LitleMagentoXMLWriter {
 							Element fileElement = doc.createElement("file");
 							// file attributes
 							setAttribute(fileElement, "name", fileOrDir.getName());
-							setAttribute(fileElement, "md5sum", getMD5Checksum(fileOrDir.getAbsolutePath()));
-							setAttribute(fileElement, "role", this.folderToRoleMap.get(originalCall));
+							setAttribute(fileElement, "hash", getMD5Checksum(fileOrDir.getAbsolutePath()));
 							
 							// add file to parentElement
 							parentElement.appendChild(fileElement);
@@ -269,20 +279,6 @@ public class LitleMagentoXMLWriter {
 					.println("Insufficient number of arguments. This build utility requires 3 arguments to work correctly: Version number, Path to a folder to copy/write files to, Path to litle-integration-magento folder.");
 			return;
 		}
-
-		System.out.println("Values being used:");
-		System.out.println("\tVersion Number: " + versionNumber);
-		System.out.println("\tPath to Copy Files to: " + pathToCopyFiles);
-		System.out.println("\tPath to litle-integration-magento: "
-				+ pathToMagentoIntegration);
-
-		// if( versionNumber.isEmpty() )
-		// versionNumber = "8.12.0";
-		// if( pathToCopyFiles.isEmpty() )
-		// pathToCopyFiles = "/usr/local/litle-home/aagarwal/MagentoBuild/";
-		// if( pathToMagentoIntegration.isEmpty() )
-		// pathToMagentoIntegration =
-		// "/usr/local/litle-home/aagarwal/git/litle-integration-magento/";
 
 		LitleMagentoXMLWriter newObj = new LitleMagentoXMLWriter(versionNumber,
 				pathToCopyFiles, pathToMagentoIntegration);
