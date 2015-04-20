4b24fb6092ffc490989dc78fab31a6d66eba3b44
Juncai <cai.jun@husky.neu.edu>
2015-04-20 12:10:22 -0400

    fix logic in Orderview page

--- app/code/local/Litle/CreditCard/Block/Adminhtml/Orderview.php
+++ app/code/local/Litle/CreditCard/Block/Adminhtml/Orderview.php
@@ -33,20 +33,17 @@ class Litle_CreditCard_Block_Adminhtml_Orderview extends Mage_Adminhtml_Block_Sa
 		
 	public function __construct() {
 		parent::__construct();
-		
-		
- 		$order = $this->getOrder();
+
+        $order = $this->getOrder();
 	    if(Mage::helper("creditcard")->isMOPLitle($order->getPayment()))
 		{
-// 			check if Auth-Reversal needs to be shown
-			if( Mage::helper("creditcard")->isStateOfOrderEqualTo($order, Mage_Sales_Model_Order_Payment_Transaction::TYPE_AUTH) &&
-				!(Mage::helper("creditcard")->isMOPLitleECheck($order->getPayment()->getData('method')))
-			)
+            if($order->getPayment()->getAuthorizationTransaction() && $order->getPayment()->getAmountPaid() == 0)
 			{
 				$message = 'Are you sure you want to reverse the authorization?';
 				$this->_updateButton('void_payment', 'label','Auth-Reversal');
 				$this->_updateButton('void_payment', 'onclick', "confirmSetLocation('{$message}', '{$this->getVoidPaymentUrl()}')");
 			}
+
 // 			check if Void-Refund needs to be shown		
 			else if( Mage::helper("creditcard")->isStateOfOrderEqualTo($order, Mage_Sales_Model_Order_Payment_Transaction::TYPE_REFUND))
 			{
@@ -61,12 +58,12 @@ class Litle_CreditCard_Block_Adminhtml_Orderview extends Mage_Adminhtml_Block_Sa
 			}
 			//check if void capture or void sale needs to be shown
 			else if(Mage::helper("creditcard")->isStateOfOrderEqualTo($order, Mage_Sales_Model_Order_Payment_Transaction::TYPE_CAPTURE) &&
-				$this->wasLastTxnLessThan48HrsAgo($order->getPayment()))
+				$this->wasLastTxnLessThan24HrsAgo($order->getPayment()))
 			{
 				$mop = $order->getPayment()->getData('method');
 				//check if paying with a credit card
-				if(Mage::helper("creditcard")->isMOPLitleCC($mop)){
-					$onclickJs = 'deleteConfirm(\''
+				if(Mage::helper("creditcard")->isMOPLitleCC($mop) || Mage::helper("creditcard")->isMOPLitlePaypal($mop)){
+                    $onclickJs = 'deleteConfirm(\''
 					. Mage::helper('sales')->__('Are you sure?  If any previous partial captures were done on this order, or if capture was not done today then do a refund instead.')
 					. '\', \'' . $this->getVoidPaymentUrl() . '\');';
 				
@@ -82,22 +79,22 @@ class Litle_CreditCard_Block_Adminhtml_Orderview extends Mage_Adminhtml_Block_Sa
 					. '\', \'' . $this->getVoidPaymentUrl() . '\');';
 					
 					$this->_addButton('void_sale', array(
-													                'label'    => 'Void Sale',
-													                'onclick'  => $onclickJs,
+                                                'label'    => 'Void Sale',
+                                                'onclick'  => $onclickJs,
 					));
 				}
 			}
 		}
 	}
 	
-	public function wasLastTxnLessThan48HrsAgo(Varien_Object $payment)
+	public function wasLastTxnLessThan24HrsAgo(Varien_Object $payment)
 	{
 		$lastTxnId = $payment->getLastTransId();
 		$lastTxn = $payment->getTransaction($lastTxnId);
 		$timeOfLastTxn = $lastTxn->getData('created_at');
 	
-		//check if last txn was less than 48 hrs ago (172800 seconds == 48 hrs)
-		return ((time()-strtotime($timeOfLastTxn)) < 172800);
+		//check if last txn was less than 24 hrs ago (86400 seconds == 24 hrs)
+		return ((time()-strtotime($timeOfLastTxn)) < 86400);
 	}
   
 }
