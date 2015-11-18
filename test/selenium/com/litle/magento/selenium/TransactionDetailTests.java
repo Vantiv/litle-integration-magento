e8a815e38ba3862fff7bef131c1d720377b3c8f1
Amit <amitvig22@gmail.com>
2015-11-18 15:49:39 -0500

    Update TransactionDetailTests.java

--- test/selenium/com/litle/magento/selenium/TransactionDetailTests.java
+++ test/selenium/com/litle/magento/selenium/TransactionDetailTests.java
@@ -24,7 +24,7 @@ public class TransactionDetailTests extends BaseTestCase {
 	    iAmDoingLitleAuth();
 	    iAmLoggedInAsWithThePassword("abc@gmail.com", "password");
 	    iHaveInMyCart("vault");
-	    iCheckOutWith("Visa", "4100000000000001");
+	    iCheckOutWith("Visa", "4100000000000000");
 	    iLogOutAsUser();
 
 	    iAmLoggedInAsAnAdministrator();
